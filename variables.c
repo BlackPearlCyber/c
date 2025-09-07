869a9c712d7f953136118293415b64e913f388f9
Muthupalirusan <118041663+BlackPearlCyber@users.noreply.github.com>
2025-09-07 13:28:32 +0530

    Adding comments in variables.c

--- variables.c
+++ variables.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> 
 int main () {
-int a = 10;
-float b = 23.4;
-printf("Integer %d  \n",a);
+int a = 10; /* Integer data type*/
+float b = 23.4;/* Float  data type*/
+ /* %d is a format specifier used to print or read integers */
+printf("Integer %d  \n",a); 
+  /* %f is another format specifier in C, used for floating-point numbers (i.e., numbers with decimals)*/
 printf("Float %f", b);
 }
